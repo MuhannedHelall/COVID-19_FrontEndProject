6c38b30ba941d3773f3cd5b00ba10d7ec104d4c8
Muhanned Helall <mohandhelall@gmail.com>
2022-10-15 16:06:43 +0200

    V2

---
+++ assets/javaScript/index.js
@@ -0,0 +1,36 @@
+function loadNumber() {
+    let numbers = document.querySelectorAll('.loadNumber');
+    numbers.forEach(number => {
+        let goal = number.textContent;
+        let counter = 0;
+        let interval = setInterval(() => {
+            if (counter >= goal) {
+                clearInterval(interval);
+                number.textContent = goal;
+            } else {
+                counter += 3124567;
+                number.textContent = counter;
+            }
+        }, 3000 / goal);
+    });
+}
+
+function toggleIcon(parent) {
+    icons = parent.parentNode.parentNode.querySelectorAll(".icons");
+    for (i = 0; i < icons.length; i++) {
+        icons[i].classList.add('bi-plus-circle');
+        icons[i].classList.remove('bi-dash-circle-fill');
+        if (parent.getAttribute("value") === "plus")
+            icons[i].parentElement.setAttribute("value", "plus");
+    }
+    if (parent.getAttribute("value") === "plus") {
+        parent.children[1].classList.remove('bi-plus-circle');
+        parent.children[1].classList.add('bi-dash-circle-fill');
+        parent.setAttribute("value", "minus");
+    }
+    else {
+        parent.children[1].classList.add('bi-plus-circle');
+        parent.children[1].classList.remove('bi-dash-circle-fill');
+        parent.setAttribute("value", "plus");
+    }
+}
